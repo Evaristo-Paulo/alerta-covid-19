d6c907bd1bce79abda641003e9af6205ae803134
MrPaul <ivarilson909@gmail.com>
2022-01-03 14:11:20 +0100

    Troca API que retorna as estatística sobre a COVID-19

--- js/covid-api.js
+++ js/covid-api.js
@@ -3,26 +3,26 @@ let today = new Date();
 //DATA DE ONTEM
 let yesterday = new Date(today.setDate(today.getDate() - 1))
 //DATA DO DIA DE ONTEM EM ANGOLA
-let currentData = yesterday.toLocaleString('pt-BR', { timeZone: 'Africa/Luanda', year: 'numeric', month: '2-digit', day: '2-digit' })
+let currentData = yesterday.toLocaleString('pt-BR', {
+    timeZone: 'Africa/Luanda',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+})
 //SUBSTITUIR / POR -
 let date = currentData.replaceAll('/', '-')
-//DIVIDIR ELEMENTOS DA DATA
-let dataVector = date.split("-");
-//ORGANIZAR EM FORMATO YYYY-MM-DD
-let from = `${dataVector[2]}-${dataVector[1]}-${dataVector[0]}`
 //DISPLAY DATA NA PÁGINA
-document.querySelector('#current-data').innerText= date
+document.querySelector('#current-data').innerText = date
 
-const url = 'https://api.covid19api.com/live/country/angola/status/confirmed/date/' + from
+const url = 'https://corona.lmao.ninja/v2/countries/Angola?yesterday&strict&query%20'
 fetch(url).then((response) => response.json())
     .then(function (data) {
-        const results = data;
-
-        return results.map(function (result) {
-            document.querySelector('#confirmedCase').innerText = result.Confirmed
-            document.querySelector('#activeCase').innerText = result.Active
-            document.querySelector('#deaths').innerText = result.Deaths
-        })
+        const result = data;
+        //console.log(result)
+        document.querySelector('#confirmedCase').innerText = result.cases
+        document.querySelector('#activeCase').innerText = result.active
+        document.querySelector('#deaths').innerText = result.deaths
+        document.querySelector('#recovered').innerText = result.recovered
 
     })
     .catch((error) => {
